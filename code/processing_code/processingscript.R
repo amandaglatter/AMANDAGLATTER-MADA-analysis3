76816176c0304ddec6e9f3a1532f74b6d9765749
Amanda Glatter <alglatter@gmail.com>
2021-10-12 17:55:43 -0400

    Processing data

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -5,21 +5,16 @@
 #and saves it as Rds file in the processed_data folder
 
 #load needed packages. make sure they are installed.
-library(readxl) #for loading Excel files
 library(dplyr) #for data processing
 library(here) #to set paths
+library(tidyverse)
 
 #path to data
 #note the use of the here() package and not absolute paths
-data_location <- here::here("data","raw_data","exampledata.xlsx")
-
+data_location <- here::here("data","raw_data","SympAct_Any_Pos.Rda")
+rawdata <- readRDS(data_location)
 #load data. 
-#note that for functions that come from specific packages (instead of base R)
-# I often specify both package and function like so
-#package::function() that's not required one could just call the function
-#specifying the package makes it clearer where the function "lives",
-#but it adds typing. You can do it either way.
-rawdata <- readxl::read_excel(data_location)
+
 
 #take a look at the data
 dplyr::glimpse(rawdata)
@@ -28,39 +23,46 @@ dplyr::glimpse(rawdata)
 #that is often not possible.
 print(rawdata)
 
-# looks like we have measurements for height (in centimeters) and weight (in kilogram)
+#Remove all variables that have Score or Total or FluA or FluB or Dxname or 
+#Activity in their name. 
 
-# there are some problems with the data: 
-# There is an entry which says "sixty" instead of a number. 
-# Does that mean it should be a numeric 60? It somehow doesn't make
-# sense since the weight is 60kg, which can't happen for a 60cm person (a baby)
-# Since we don't know how to fix this, we need to remove the person.
-# This "sixty" entry also turned all Height entries into characters instead of numeric.
-# We need to fix that too.
-# Then there is one person with a height of 6. 
-# that could be a typo, or someone mistakenly entered their height in feet.
-# Since we unfortunately don't know, we'll have to remove this person.
-# similarly, there is a person with weight of 7000, which is impossible,
-# and one person with missing weight.
-# to be able to analyze the data, we'll remove those 5 individuals
 
-# this is one way of doing it. Note that if the data gets updated, 
-# we need to decide if the thresholds are ok (newborns could be <50)
+cleandata <- rawdata %>% dplyr::select(
+  -contains("Score"),
+  -contains("Total"),
+  -contains("FluA"),
+  -contains("FluB"),
+  -contains("Dxname"),
+  -contains("Activity")
+)
 
-processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>% 
-                             dplyr::mutate(Height = as.numeric(Height)) %>% 
-                             dplyr::filter(Height > 50 & Weight < 1000)
+#Also remove the variable Unique.Visit. 
+cleandata <- cleandata %>% subset(select = -c(Unique.Visit))
 
-# save data as RDS
-# I suggest you save your processed and cleaned data as RDS or RDA/Rdata files. 
-# This preserves coding like factors, characters, numeric, etc. 
-# If you save as CSV, that information would get lost.
-# See here for some suggestions on how to store your processed data:
-# http://www.sthda.com/english/wiki/saving-data-into-r-data-format-rds-and-rdata
+#You should be left with 32 variables coding for presence or absence of 
+#some symptom. Only one, temperature, is continuous. 
+#Let's check this to confirm.
+
+summary(cleandata)
+#This checks out with the statement that only Temperature is continuous.
+ncol(cleandata)
+#Yes, there are 32. 
+
+#A few have multiple categories. Remove any NA observations, there aren't many.
+
+cleandata <- cleandata %>% drop_na()
+
+#At the end of your (fairly short) cleaning process, you should end up with
+#730 observations and 32 variables.
+
+ncol(cleandata)
+nrow(cleandata)
+
+#And that's it! All clean.
 
 # location to save file
 save_data_location <- here::here("data","processed_data","processeddata.rds")
 
-saveRDS(processeddata, file = save_data_location)
+saveRDS(cleandata, file = save_data_location)
 
 
